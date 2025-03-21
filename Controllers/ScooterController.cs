f7213a272fe7ede71640245dc191f3efd1f99b73
Ajeet_Yadav <CodewithAjeetyadav@gmail.com>
2025-03-21 17:46:36 +0530

    Done changes for Logic change

--- Controllers/ScooterController.cs
+++ Controllers/ScooterController.cs
@@ -24,11 +24,31 @@ namespace ScooterApplication.Controllers
                 //int totalLife = model.Wheel1 + model.Wheel2 + model.Spare;
                 //model.MaxDistance = Math.Min(model.Wheel1, Math.Min(model.Wheel2, model.Spare));
                 //model.MaxDistance = (double)totalLife * 2 / 3;
-                int[] tires = { model.Wheel1, model.Wheel2, model.Spare }; // Example lifecycles
 
-                double totalLifecycle = tires.Sum();   // Sum of all tire lifecycles
-                //var test = totalLifecycle / 2;
-                model.MaxDistance = totalLifecycle / 2;
+                if(model.Wheel1 ==0 && model.Wheel2 == 0)
+                {
+                    model.MaxDistance = 0;
+                    return View("ScooterWheelLife", model);
+                }
+                double[] tires = { model.Wheel1, model.Wheel2, (double)model.Spare }; // Example lifecycles
+                Array.Sort(tires);
+                double total = 0;
+                var firstPart = tires[0] / 2;
+                total += firstPart;
+                tires[0] = tires[0] - firstPart;
+                tires[1] = tires[1] - firstPart;
+                //----------------------------------------
+                tires[2] = tires[2] - tires[1];
+                total += tires[1];
+                //--------------
+                total += Math.Min(tires[0], tires[2]);
+
+                model.MaxDistance = total;
+                //int[] tires = { model.Wheel1, model.Wheel2, model.Spare ?? 0 }; // Example lifecycles
+
+                //double totalLifecycle = tires.Sum();   // Sum of all tire lifecycles
+                ////var test = totalLifecycle / 2;
+                //model.MaxDistance = totalLifecycle / 2;
             }
             return View("ScooterWheelLife", model);
         }
--- Models/ScooterWheelLife.cs
+++ Models/ScooterWheelLife.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,13 @@ namespace ScooterApplication.Models
 {
     public class ScooterWheelLife
     {
-         
+        [Required(ErrorMessage = "Wheel 1 is required.")]
         public int Wheel1 { get; set; }
+
+        [Required(ErrorMessage = "Wheel 2 is required.")]
         public int Wheel2 { get; set; }
-        public int Spare { get; set; }
+
+        public int? Spare { get; set; }
         public double? MaxDistance { get; set; }
     
 }
